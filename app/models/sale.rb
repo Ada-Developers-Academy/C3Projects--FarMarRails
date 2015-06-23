fcc3a489653e74c2ad88d591c65649e7af94e3ee
Shanna Chau <shannachau@gmail.com>
2015-06-23 15:06:26 -0700

    Created associations between models

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,4 @@
 class Sale < ActiveRecord::Base
+  belongs_to :vendor
+  belongs_to :product
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,5 @@
 class Vendor < ActiveRecord::Base
+  belongs_to :market
+  has_many :sales
+  has_many :products
 end
