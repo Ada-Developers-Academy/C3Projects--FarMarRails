cd13b6f2052359f2eb6aa50d0b29f1cfb03d25b7
Brenna <brennaleker@gmail.com>
2015-06-23 16:17:14 -0700

    set seed file up for our csvs

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'markets#index'
 
+resources :markets do
+  resources :vendors do
+    resources :sales
+    resources :products
+  end
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
