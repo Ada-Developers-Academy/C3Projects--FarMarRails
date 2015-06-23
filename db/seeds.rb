18168318bad91380a47f69823e5bb292a88dd882
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-23 15:37:31 -0700

    finished writing CSV import code

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,26 @@
 
 require 'csv'
 
-headers = [ :id, :name, :address, :city, :county, :state, :zip ]
+market_headers = [ :id, :name, :address, :city, :county, :state, :zip ]
 
-CSV.foreach("seed_csvs/markets.csv", encoding: "UTF-8", write_headers: true, headers: headers ) do |row|
+CSV.foreach("seed_csvs/markets.csv", encoding: "UTF-8", write_headers: true, headers: market_headers ) do |row|
   Market.create(row.to_hash)
+end
+
+vendor_headers = [ :id, :name, :num_employees, :market_id ]
+
+CSV.foreach("seed_csvs/vendors.csv", encoding: "UTF-8", write_headers: true, headers: vendor_headers ) do |row|
+  Vendor.create(row.to_hash)
+end
+
+product_headers = [ :id, :name, :vendor_id ]
+
+CSV.foreach("seed_csvs/products.csv", encoding: "UTF-8", write_headers: true, headers: product_headers ) do |row|
+  Product.create(row.to_hash)
+end
+
+sale_headers = [ :id, :amount, :purchase_time, :vendor_id, :product_id ]
+
+CSV.foreach("seed_csvs/sales.csv", encoding: "UTF-8", write_headers: true, headers: sale_headers ) do |row|
+  Sale.create(row.to_hash)
 end
