4f3deb5ba1c79cc89b2919ff7c14742feb87b155
Shanna Chau <shannachau@gmail.com>
2015-06-23 15:58:03 -0700

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :markets do
+    resources :vendors
+  end
+
+  resources :vendors do
+    resources :products
+    resources :sales
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
