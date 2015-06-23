44f169149eefdd4af78c267a554d2e7e0bb5e1a7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-23 16:42:43 -0700

    added markets#index and #show

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,24 @@
 class MarketsController < ApplicationController
+  before_action :set_market, only: [:show]
+
+  def index
+    @markets = Market.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def set_market
+      @market = Market.find(params[:id])
+    end
 end
