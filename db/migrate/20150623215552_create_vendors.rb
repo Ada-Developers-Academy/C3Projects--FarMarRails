65d70becd37ade44a7b3f96a46c6284a311e9238
Ashley Watkins <AWatkins31@gmail.com>
2015-06-23 15:01:38 -0700

    Creates initial models for vendor, product, and sales. Also added associations.

---
+++ db/migrate/20150623215552_create_vendors.rb
@@ -0,0 +1,11 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.string :name
+      t.string :num_employees
+      t.integer :market_id
+
+      t.timestamps null: false
+    end
+  end
+end
