a145744fc1e14727328c9fbf06d93fc1fd223893
Katie Atrops <katrops@gmail.com>
2015-06-23 15:29:10 -0700

    added model, migrate, and seed stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach("db/../seed_csvs/markets.csv") do |row|
+
+  Market.create(
+    name: row[1],
+    address: row[2],
+    city: row[3],
+    state: row[4],
+    county: row[5],
+    zip: row[6]
+    )
+
+end
+
+CSV.foreach("db/../seed_csvs/products.csv") do |row|
+
+  Product.create(
+    name: row[1],
+    vendor_id: row[2]
+    )
+
+end
+
+CSV.foreach("db/../seed_csvs/vendors.csv") do |row|
+
+  Vendor.create(
+    name: row[1],
+    num_of_employees: row[2],
+    market_id: row[3]
+    )
+end
+
+CSV.foreach("db/../seed_csvs/sales.csv") do |row|
+
+  Sale.create(
+    amount: row[1],
+    purchase_time: row[2],
+    vendor_id: row[3],
+    product_id: row[4]
+    )
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
