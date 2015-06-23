a219f67118e61b46f21987da08358e7442b69ca7
Jeri <drvonnjerryxlii@gmail.com>
2015-06-23 14:33:13 -0700

    setting up baseline

---
+++ db/migrate/20150623212440_create_sales.rb
@@ -0,0 +1,12 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.integer :amount
+      t.datetime :purchase_time
+      t.integer :vendor_id
+      t.integer :product_id
+
+      t.timestamps null: false
+    end
+  end
+end
