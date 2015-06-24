e52d8426d2f977cba4ceb47bc40a31c8b0e154ac
Loraine <loraine@lorainekv.com>
2015-06-24 14:17:53 -0700

    Created new product as a vendor feature.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+  def new
+
+    @product = Product.new
+  end
+
+  def create
+
+    @product = Product.create(create_params[:product])
+
+    redirect_to vendor_path(@product.vendor_id)
+  end
+
+
+  private
+
+  def create_params
+    params.permit(product: [:name, :vendor_id, :id])
+
+  end
+
+
 end
