cc039bf72eb3386ba7c22b1ee6f27dd5ba374b4e
Lila Garcia <lilagrc@gmail.com>
2015-06-24 12:00:26 -0700

    Added products index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
