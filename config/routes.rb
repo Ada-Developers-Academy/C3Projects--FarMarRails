05ceb28386574368bbd3f9d1d44af10177c4ef06
Brenna <brennaleker@gmail.com>
2015-06-24 09:46:28 -0700

    fixed routing issues and added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'markets#index'
+  root 'users#index'
 
 resources :markets do
   resources :vendors do
