bf6634d758d274115fd8efc11c87fa692373a9d6
Elia <elgrenier@gmail.com>
2015-06-24 12:25:34 -0700

    Added new and edit forms

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -15,4 +15,35 @@ class MarketsController < ApplicationController
     @header = "Market Details"
     @market = Market.find(params[:id])
   end
+
+  def new
+    @header= "Add a New Market"
+    @adj = "Add"
+    @market = Market.new
+  end
+
+  def create
+    @market = Market.new(create_params[:market])
+    @market.save
+    redirect_to markets_url
+  end
+
+  def edit
+    @header = "Edit Market"
+    @adj = "Update"
+    @market = Market.find(params[:id])
+  end
+
+  def update
+    @market = Market.find(params[:id])
+    @market.update(create_params[:market])
+    redirect_to markets_url
+  end
+
+  private
+
+  def create_params
+    params.permit(market: [:name, :address, :city, :county, :state, :zip])
+  end
+
 end
