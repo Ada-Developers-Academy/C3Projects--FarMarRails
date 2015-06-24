d10ac814b453fe4a67a4376bc1dfd20c03009ad6
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-24 16:48:07 -0700

    Added functionality to add new & delete products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,18 @@
 class ProductsController < ApplicationController
 
+  def create
+    @product = Product.new(permit_params[:product])
+    @product.save
+
+    redirect_to(vendor_path(Vendor.find(@product.vendor_id)))
+  end
+
+  def new
+    @vendor_id = params[:vendor_id]
+    @product = Product.new()
+    @product.vendor_id = @vendor_id
+  end
+
   def edit #GET one market to modify.
    @product = Product.find(params[:id])
   end
@@ -9,6 +22,12 @@ class ProductsController < ApplicationController
     @product.name = permit_params[:product][:name]
     @product.vendor_id = permit_params[:product][:vendor_id]
     @product.save
+
+    redirect_to(vendor_path(Vendor.find(@product.vendor_id)))
+  end
+
+  def destroy
+    @product = Product.find(params[:id]).destroy
 
     redirect_to(vendor_path(Vendor.find(@product.vendor_id)))
   end
