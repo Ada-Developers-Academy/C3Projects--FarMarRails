66860222afde8ff29805d06a44cd088fea013402
marleigh <camarleigh@gmail.com>
2015-06-24 10:07:06 -0700

    created add market methods and form.

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -4,4 +4,22 @@ class MarketsController < ApplicationController
     @markets = Market.all
   end
 
+  def create
+    @market = Market.new(permit_params[:market])
+    @market.save
+
+    redirect_to(markets_path)
+  end
+
+  def new
+    @market = Market.new()
+  end
+
+  private
+
+  def permit_params
+    params.permit(market:[:name,:street_address,:city, :county, :state, :zip])
+  end
+
+
 end
