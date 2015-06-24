837ea97b05ff84bbb27472dfa985a613bf0ff8f2
Shanna Chau <shannachau@gmail.com>
2015-06-24 14:13:54 -0700

    Got form to populate with correct market id

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -11,7 +11,16 @@ class VendorsController < ApplicationController
   end
 
   def new
+    @vendor = Vendor.new
 
+    render :add_vendor
+  end
+
+  def create
+    @vendor = Vendor.new(create_params[:vendor])
+    @market = Market.find(params[:id])
+
+    render "markets/show"
   end
 
   def edit
@@ -24,20 +33,18 @@ class VendorsController < ApplicationController
     @vendor = Vendor.find(params[:id])
       new_name            = params[:vendor][:name]
       new_no_of_employees = params[:vendor][:no_of_employees]
-      new_market_id       = params[:vendor][:market_id]
 
 
     @vendor.update(name:            new_name,
-                   no_of_employees: new_no_of_employees,
-                   market_id:       new_market_id,
+                   no_of_employees: new_no_of_employees
                   )
-    @market = Market.find(params[:id])              
+    @market = Market.find(params[:id])
     render :show
   end
 
   private
 
   def create_params
-    params.permit(vendor: [:name, :no_of_employees, :market_id])
+    params.permit(vendor: [:name, :no_of_employees])
   end
 end
