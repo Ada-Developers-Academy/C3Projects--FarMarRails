dd177643d9aa5e2b9f3966f095fa0c4a5a4c6b08
marleigh <camarleigh@gmail.com>
2015-06-24 13:47:43 -0700

    added edit/update actions to vendor controller. Can edit venor from mkt page.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -14,4 +14,30 @@ class VendorsController < ApplicationController
     @vendor = Vendor.find(params[:id])
   end
 
+  def new
+    @vendor = Vendor.new
+
+  end
+  
+  def edit
+    @vendor = Vendor.find(params[:id]) 
+  
+  end
+  
+  def update
+    @vendor = Vendor.find(params[:id])
+    @vendor.name = permit_params[:vendor][:name]
+    @vendor.no_of_employees = permit_params[:vendor][:no_of_employees]
+    @vendor.market_id = permit_params[:vendor][:market_id]
+    @vendor.save
+
+    redirect_to(market_path(Market.find(@vendor.market_id)))
+  end
+
+private
+
+  def permit_params
+    params.permit(vendor:[:name,:no_of_employees,:market_id])
+  end
+
 end
