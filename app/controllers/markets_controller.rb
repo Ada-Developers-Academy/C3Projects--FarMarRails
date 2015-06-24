2baee71b29a087417ba007ebc2e58a6cc00cf105
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-06-23 17:46:45 -0700

    WIP: market => routes, index and show.

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,23 @@
 class MarketsController < ApplicationController
+
+  def index
+    @all_markets = Market.all
+  end
+
+  def show
+    find_market
+  end
+
+private
+
+  def find_market
+      id = params[:id]
+      @market = Market.find(id)
+  end
+
+  def create_params
+    params_permit(:market [:name, :id])
+  end
+
+
 end
