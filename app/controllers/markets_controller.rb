81154fc8ee6a12a7207864368a0975f50beac03f
Shanna Chau <shannachau@gmail.com>
2015-06-24 10:27:01 -0700

    Created edit page for markets

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -22,7 +22,29 @@ class MarketsController < ApplicationController
   end
 
   def edit
-    
+    @market = Market.find(params[:id])
+
+    render :edit_market
+  end
+
+  def update
+    @market = Market.find(params[:id])
+      new_name    = params[:market][:name]
+      new_address = params[:market][:address]
+      new_city    = params[:market][:city]
+      new_county  = params[:market][:county]
+      new_state   = params[:market][:state]
+      new_zip     = params[:market][:zip]
+
+    @market.update(name:    new_name,
+                   address: new_address,
+                   city:    new_city,
+                   county:  new_county,
+                   state:   new_state,
+                   zip:     new_zip
+                  )
+
+    render :show
   end
 
   private
