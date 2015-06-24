6def88d3fa7f9821d3995a31766dd4ba1b1d8bd3
Lila Garcia <lilagrc@gmail.com>
2015-06-24 14:25:15 -0700

    Added create new product for vendor functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,20 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @vendor = Vendor.find(params[:vendor_id])
+    @products = @vendor.products
   end
 
   def new
-
+    @vendor = Vendor.find(params[:vendor_id])
+    @product = Product.new
   end
 
   def create
+    @vendor = Vendor.find(params[:vendor_id])
+    @product = Product.create(create_params[:product])
 
+    redirect_to vendor_products_path
   end
 
   def edit
@@ -19,4 +24,11 @@ class ProductsController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def create_params
+    params.permit(product: [:name, :vendor_id])
+  end
+
 end
