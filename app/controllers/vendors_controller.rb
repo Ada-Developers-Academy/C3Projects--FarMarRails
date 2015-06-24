f4c3a238b2b935df9d8ea603f0281c88ae6b5550
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-24 14:23:18 -0700

    Added ability to add vendor to market.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -14,16 +14,25 @@ class VendorsController < ApplicationController
     @vendor = Vendor.find(params[:id])
   end
 
+  def create
+    @vendor = Vendor.new(permit_params[:vendor])
+    @vendor.save
+
+    redirect_to(market_path(market_of_vendor))
+  end
+
   def new
+    @market_id = params[:market_id]
     @vendor = Vendor.new
+    @vendor.market_id = @market_id
 
   end
-  
+
   def edit
-    @vendor = Vendor.find(params[:id]) 
-  
+    @vendor = Vendor.find(params[:id])
+
   end
-  
+
   def update
     @vendor = Vendor.find(params[:id])
     @vendor.name = permit_params[:vendor][:name]
@@ -31,13 +40,17 @@ class VendorsController < ApplicationController
     @vendor.market_id = permit_params[:vendor][:market_id]
     @vendor.save
 
-    redirect_to(market_path(Market.find(@vendor.market_id)))
+    redirect_to(market_path(market_of_vendor))
   end
 
 private
 
   def permit_params
     params.permit(vendor:[:name,:no_of_employees,:market_id])
+  end
+
+  def market_of_vendor
+    Market.find(@vendor.market_id)
   end
 
 end
