64fcb79694f362144101dd0fbc2a05835dab9596
Ashley Watkins <AWatkins31@gmail.com>
2015-06-24 15:46:34 -0700

    Creates links + functionality to add a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,26 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
+  before_action :set_vendor, only: [:new, :create]
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.create(product_params)
+    redirect_to vendor_product_path(@vendor, product)
+  end
 
   private
     def set_product
       @product = Product.find(params[:id])
+    end
+
+    def set_vendor
+      @vendor = Vendor.find(params[:vendor_id])
+    end
+
+    def product_params
+      params.permit(product: [:name, :vendor_id])[:product]
     end
 end
