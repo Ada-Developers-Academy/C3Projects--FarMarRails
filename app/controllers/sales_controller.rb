a20efc8c7ec38bbb26cde98e29443095754c7a30
Lindsey Reno <lindseyannereno@gmail.com>
2015-06-24 16:51:38 -0700

    Added ability to create a new sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,12 +1,27 @@
 class SalesController < ApplicationController
 
   def index
+    @vendor = Vendor.find(params[:vendor_id])
+    @vendor_sales = @vendor.sales
+    render :index
   end
 
   def create
+    @vendor = Vendor.find(params[:vendor_id])
+
+    @url = "/vendors/#{@vendor.id}/sales"
+    @sale = Sale.new(create_params[:sale])
+    @sale.save
+
+    redirect_to "/vendors/#{@vendor.id}/sales"
   end
 
   def new
+    @vendor = Vendor.find(params[:vendor_id])
+    @sale = Sale.new
+    @url = "/vendors/#{@vendor.id}/sales"
+
+    render :new
   end
 
   def edit
@@ -18,4 +33,9 @@ class SalesController < ApplicationController
   def update
   end
 
+  private
+
+  def create_params
+    params.permit(sale: [:amount, :purchase_time, :product_id, :vendor_id])
+  end
 end
