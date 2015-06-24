78ebd9196a16683e8d2e5e4dd8cf5d2581836909
Loraine <loraine@lorainekv.com>
2015-06-24 10:03:27 -0700

    Created a new vendor form that is accessible from each market page.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,21 @@
 class VendorsController < ApplicationController
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.create(create_params[:vendor])
+
+    redirect_to market_path(market_id)
+
+  end
+
+  private
+
+  def create_params
+    params.permit(vendor: [:name, :no_of_employees, :market_id])
+
+  end
+
 end
