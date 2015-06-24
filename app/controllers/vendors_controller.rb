f5da47f58e5257451c4bbcf20d97348530a79827
Anita <asovay@gmail.com>
2015-06-24 15:57:43 -0700

    added 'Add a product form' and corrected variables in vendor controller

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -15,25 +15,24 @@ class VendorsController < ApplicationController
   end
 
   # Edit an existing vendor
-  def update
-    @market = Market.find(params[:id])
-    @name = market_params[:market][:name]
-    @address = market_params[:market][:address]
-    @city = market_params[:market][:city]
-    @county = market_params[:market][:county]
-    @state = market_params[:market][:state]
-    @zip = market_params[:market][:zip]
-
-    @market.update(name: @name)
-    @market.update(address: @address)
-    @market.update(city: @city)
-    @market.update(county: @county)
-    @market.update(state: @state)
-    @market.update(zip: @zip)
-    @market.save
-
-    redirect_to :markets
+  def edit
+    @vendor = Vendor.find(params[:id])
+    render :edit
   end
+
+  def update
+    @vendor = vendor.find(params[:id])
+    @name = vendor_params[:market][:name]
+    @no_of_employees = vendor_params[:market][:no_of_employees]
+
+    @vendor.update(name: @name)
+    @vendor.update(no_of_employees: @no_of_employees)
+    @vendor.save
+
+    redirect_to :vendors
+  end
+
+  private
 
   def vendor_params
     params.permit(vendor: [:name, :no_of_employees, :market_id])
