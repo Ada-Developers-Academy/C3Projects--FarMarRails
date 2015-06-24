80c3d0fd072b470c4a442fceb7b0d1797067b096
Katie Atrops <katrops@gmail.com>
2015-06-24 16:09:08 -0700

    added a new sale for a vendor

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,17 @@
 class SalesController < ApplicationController
+
+  def new
+    @sale = Sale.new
+    @vendor = Vendor.find(params[:vendor_id])
+    @products = @vendor.products
+  end
+
+  def create
+    @sale = Sale.create(sale_params)
+    @sale.save
+    redirect_to vendor_sales_path
+  end
+
   def index
     @vendor = Vendor.find(params[:vendor_id])
     @sales = @vendor.sales.to_a
@@ -7,8 +20,21 @@ class SalesController < ApplicationController
 
   def sales_of_month
     @vendor = Vendor.find(params[:vendor_id])
-    @sales = @vendor.sales.to_a
+    @sales_collection = @vendor.sales.to_a
+    @sales = Sale.sales_by_month(@sales_collection)
     render :sales_of_month
     
   end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(:amount, :purchase_time, :vendor_id, :product_id)
+  end
 end
+
+
+
+
+
+
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,5 +2,14 @@ class Sale < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :product
 
+  def self.sales_by_month(sales)
+    sales_array = []
+    sales.each do |sale|
+      if sale.purchase_time.month == Time.now.month
+        sales_array.push(sale)
+      end
+    end
+    sales_array
+  end
 
 end
