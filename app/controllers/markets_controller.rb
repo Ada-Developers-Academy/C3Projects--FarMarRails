9c06a5cc33ad96aed56d471ab382df16865fabe7
Michelle McCarthy <michellemccarthy@hotmail.com>
2015-06-24 11:45:05 -0700

    added ability to add or edit a market

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -4,26 +4,45 @@ class MarketsController < ApplicationController
   end
 
   def create
-
+#POST
+    @markets = Market.all
+    @new = Market.new(create_params[:market])
+    @new.save
+    render :index
   end
 
   def new
-
+#GET
+    @new = Market.new
   end
 
   def edit
-
+    @edit = Market.find(params[:id])
+    render :edit
   end
 
   def show
     @market = Market.find(params[:id])
     @vendors = @market.vendors
+    render :show
   end
 
   def update
-
+    @market = Market.find(params[:id])
+    @market.name = create_params[:market][:name]
+    @market.address= create_params[:market][:address]
+    @market.city = create_params[:market][:city]
+    @market.county = create_params[:market][:county]
+    @market.state = create_params[:market][:state]
+    @market.zip = create_params[:market][:zip]
+    @market.save
+    redirect_to "/markets/#{@market.id}"
   end
 
+private
 
+def create_params
+  params.permit(market: [:name, :address, :city, :county, :state, :zip, :vendor_id])
+end
 
 end
