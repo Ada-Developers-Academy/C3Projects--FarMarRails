e3d08113216f5744c741b5463d98c941d893ada5
Kyna <khn710@gmail.com>
2015-06-24 12:09:05 -0700

    added create, edit and delete for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,42 @@ class ProductsController < ApplicationController
     @vendor = Vendor.find(params[:vendor_id])
     @products = @vendor.products.to_a
   end
+
+  def new
+    @product = Product.new
+    @url = vendor_products_path
+    @method = :post
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to vendor_products_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @url = vendor_product_path
+    @method = :patch
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    @product.save
+    redirect_to vendor_products_path
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to vendor_products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :vendor_id)
+  end
+
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,8 @@
 class Sale < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :product
+
+  # def cents_to_dollars
+  #   
+  # end
 end
