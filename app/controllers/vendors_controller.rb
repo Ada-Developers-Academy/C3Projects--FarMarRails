d9caecad7234edb3acc876f496e4bf2836a62aeb
Anita <asovay@gmail.com>
2015-06-24 13:59:35 -0700

    Added edit a market form --working. Starting working on edit a vendor form

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,6 +3,7 @@ class VendorsController < ApplicationController
   def index
   end
 
+  # Add a new vendor
   def new
     @vendor = Vendor.new(vendor_params[:vendor])
   end
@@ -10,7 +11,28 @@ class VendorsController < ApplicationController
   def create
     @vendor = Vendor.new(vendor_params[:vendor])
     @vendor.save
-    redirect_to :market_show
+    redirect_to :index
+  end
+
+  # Edit an existing vendor
+  def update
+    @market = Market.find(params[:id])
+    @name = market_params[:market][:name]
+    @address = market_params[:market][:address]
+    @city = market_params[:market][:city]
+    @county = market_params[:market][:county]
+    @state = market_params[:market][:state]
+    @zip = market_params[:market][:zip]
+
+    @market.update(name: @name)
+    @market.update(address: @address)
+    @market.update(city: @city)
+    @market.update(county: @county)
+    @market.update(state: @state)
+    @market.update(zip: @zip)
+    @market.save
+
+    redirect_to :markets
   end
 
   def vendor_params
