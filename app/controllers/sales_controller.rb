6845312b6a80d934ed0fc918e2ee49c2045cfca3
Ashley Watkins <AWatkins31@gmail.com>
2015-06-25 10:35:44 -0700

    WIP -- creating a form for sale.
    Current dilemmas: where to access the sale form, and how to determine product_id.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,21 @@
 class SalesController < ApplicationController
+  # before_action :set_sale, only: []
+  before_action :set_vendor, only: [:new, :create]
+
+  private
+    # def set_sale
+    #   @sale = Sale.find(params[:id])
+    # end
+
+    def set_vendor
+      @vendor = Vendor.find(params[:vendor_id])
+    end
+
+    # TODO: decide if we need this
+    # def set_product
+    # end
+
+    def sale_params
+      params.permit(sale: [:amount, :purchase_time, :vendor_id, :product_id])[:sale]
+    end
 end
