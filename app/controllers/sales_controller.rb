2514cca2e0e40747bd8a02b56c55dac2bae51c91
Jeri <drvonnjerryxlii@gmail.com>
2015-06-25 11:17:00 -0700

    finished barebones create sale form

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -21,10 +21,14 @@ class SalesController < ApplicationController
 
   def new
     @sale = Sale.new
+    @vendor = Vendor.find(params[:vendor_id])
   end
 
   def create
-    sale = Sale.new(create_params[:sale])
+    params = create_params[:sale]
+    params[:amount] = convert_dollars_to_cents(params[:amount])
+
+    sale = Sale.new(params)
     sale.save
 
     redirect_to "/vendors/#{ sale.vendor_id }/sales"
@@ -33,9 +37,12 @@ class SalesController < ApplicationController
   private
 
   def create_params
-    params[:sale][:amount] *= 100
-
     params.permit(sale: [:amount, :purchase_time, :vendor_id,  :product_id])
+  end
+
+  def convert_dollars_to_cents(dollar_amount)
+    cent_amount = dollar_amount.to_f * 100
+    cent_amount = cent_amount.to_i
   end
 
   def month_sales(vendor)
