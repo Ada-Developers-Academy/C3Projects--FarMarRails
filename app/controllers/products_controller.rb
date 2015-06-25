7fb7f7cb5b8d34ecbd80a8973dc6efd38a4ac3b6
Victoria <vikshab2010@hotmail.com>
2015-06-25 10:46:55 -0700

    Workind 'edit product' for vendor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,29 @@ class ProductsController < ApplicationController
     @vendor = Vendor.find(params[:product][:vendor_id])
     @products = @vendor.products
 
-    render :show
+    render :index
   end
 
+  def edit
+    @product = Product.find(params[:id])
 
+    render :edit_product
+  end
+
+  def update
+    @product = Product.find(params[:id])
+      new_name = params[:product][:name]
+      new_vendor_id = params[:product][:vendor_id]
+
+    @product.update(
+                    name:      new_name,
+                    vendor_id: new_vendor_id
+    )
+    @vendor = Vendor.find(params[:product][:vendor_id])
+    @products = @vendor.products
+    
+    render :index
+  end
 
   private
 
