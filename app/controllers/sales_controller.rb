089ae990248247346cbf0c28867454be61f4054b
Shanna Chau <shannachau@gmail.com>
2015-06-25 13:36:49 -0700

    Display a list of sales and total for vendor

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,9 +1,16 @@
 class SalesController < ApplicationController
   def index
-    @sales = Product.find(params[:product_id]).sales
-    @product = Product.find(params[:product_id])
-    @vendor = @product.vendor
+    # coming from vendors
+    @products = Vendor.find(params[:vendor_id]).products
+    @sales = Vendor.find(params[:vendor_id]).sales
+    @total_sales = add_sale_amounts(@sales)
 
+    # coming from vendors > products > sales
+    if params[:product_id]
+      @sales = Product.find(params[:product_id]).sales
+      @product = Product.find(params[:product_id])
+      @vendor = @product.vendor
+    end
     render :index
   end
 
@@ -23,11 +30,18 @@ class SalesController < ApplicationController
     render :index
   end
 
-
   private
 
   def create_params
     params.permit(sale: [:amount, :purchase_time, :vendor_id, :product_id])
+  end
+
+  def add_sale_amounts(all_sales)
+    total = 0
+    all_sales.each do |sale|
+      total += sale.amount
+    end
+    return total
   end
 
 
