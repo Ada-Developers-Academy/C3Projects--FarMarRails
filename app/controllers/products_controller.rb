8716f4eb76ee8f218c6f75a36a2ebb8e8ddaee59
Corinne Pingul <corinnepingul@gmail.com>
2015-06-26 11:59:49 -0700

    Added a comment.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,9 +28,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    vendor_id = params[:id]
+    # we will first destroy all sales assoc. with this product
     @sales = Sale.where(product_id: params[:product_id])
     @sales.destroy_all
+
+    vendor_id = params[:id]
     Product.destroy(params[:product_id])
 
     redirect_to controller: "vendors", action: "products", id: vendor_id
