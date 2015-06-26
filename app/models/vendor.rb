65ed3000e33f7a75e66386027844931fb0fef13c
Jeri <drvonnjerryxlii@gmail.com>
2015-06-26 09:44:19 -0700

    added methods to display sales reverse chronologically

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -5,4 +5,45 @@ class Vendor < ActiveRecord::Base
 
   validates_presence_of :name, :number_of_employees, :market_id
   validates_numericality_of :number_of_employees, :market_id
+
+  def recent_sales_first(how_many=sales.count)
+    sorted_sales = sales.sort_by { |sale| sale.purchase_time }
+    sorted_sales.reverse!
+
+    return sorted_sales[0, how_many]
+  end
+
+  def total_sales
+    all_sales = sales
+    amounts = all_sales.map { |sale| sale.amount }
+    total = amounts.inject { |sum, n| sum + n }
+
+    return total
+  end
+
+  def month_sales
+    current_month = Time.now.month
+    current_month_sales = []
+
+    sales.each do |sale|
+      current_month_sales.push(sale) if sale.purchase_time.month == current_month
+    end
+
+    return current_month_sales
+  end
+
+  def month_recent_sales_first
+    sorted_sales = month_sales.sort_by { |sale| sale.purchase_time }
+    sorted_sales.reverse!
+    
+    return sorted_sales.slice(0, how_many)
+  end
+
+  def month_total_sales
+    all_sales = sales
+    amounts = month_sales.map { |sale| sale.amount }
+    total = amounts.inject { |sum, n| sum + n }
+
+    return total
+  end
 end
