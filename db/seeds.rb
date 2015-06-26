4de38072bc816732c6d658c5ca507ef4263a2f5a
Lila Garcia <lilagrc@gmail.com>
2015-06-26 11:09:01 -0700

    Changed seed file to account for heroku pg database weirdness

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,41 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
-headers = [:id, :name, :address, :city, :county, :state, :zip]
 
-CSV.foreach("db/csv/markets.csv", { encoding: "UTF-8", write_headers: true, headers: headers, converters: :all}) do |row|
-  Market.create(row.to_hash)
+CSV.foreach("db/csv/markets.csv") do |row|
+  Market.create!({
+    name: row[1],
+    address: row[2],
+    city: row[3],
+    county: row[4],
+    state: row[5],
+    zip: row[6],
+  })
 end
 
-product_headers = [:id, :name, :vendor_id]
 
-CSV.foreach("db/csv/products.csv", { encoding: "UTF-8", write_headers: true, headers: product_headers, converters: :all}) do |row|
-  Product.create(row.to_hash)
+CSV.foreach("db/csv/products.csv") do |row|
+  Product.create!({
+    name: row[1],
+    vendor_id: row[2],
+  })
 end
 
-vendor_headers = [:id, :name, :num_employees, :market_id]
 
-CSV.foreach("db/csv/vendors.csv", { encoding: "UTF-8", write_headers: true, headers: vendor_headers, converters: :all}) do |row|
-  Vendor.create(row.to_hash)
+CSV.foreach("db/csv/vendors.csv") do |row|
+  Vendor.create!({
+    name: row[1],
+    num_employees: row[2],
+    market_id: row[3],
+  })
 end
 
-sale_headers = [:id, :amount, :purchase_time, :vendor_id, :product_id]
 
-CSV.foreach("db/csv/sales.csv", { encoding: "UTF-8", write_headers: true, headers: sale_headers, converters: :all}) do |row|
-  Sale.create(row.to_hash)
+CSV.foreach("db/csv/sales.csv") do |row|
+  Sale.create!({
+    amount: row[1],
+    purchase_time: row[2],
+    vendor_id: row[3],
+    product_id: row[4],
+  })
 end
