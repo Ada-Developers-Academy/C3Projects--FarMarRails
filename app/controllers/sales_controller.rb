004403b1a8cc46e5f2be71e3fdbe75667e2e89b3
Corinne Pingul <corinnepingul@gmail.com>
2015-06-26 10:20:42 -0700

    Ability to add a sale. Need to make vendor_id and sale_id hidden values in the form so that the new sale is only associated with the specific vendor and product.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,19 @@
 class SalesController < ApplicationController
+  def new
+    @sale = Sale.new
+
+    render 'sales/new'
+  end
+
+  def create
+    Sale.create(create_sale_params[:sale])
+
+    redirect_to sales_vendor_path(id: create_sale_params[:sale][:vendor_id])
+  end
+
+  private
+
+  def create_sale_params
+    params.permit(sale: [:amount, :purchase_time, :vendor_id, :product_id])
+  end
 end
