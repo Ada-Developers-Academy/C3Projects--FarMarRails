af3344a0b56299f827b7cdbb33649c553aeb18bd
Lila Garcia <lilagrc@gmail.com>
2015-06-28 13:01:42 -0700

    added nav to search pages

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,8 +1,6 @@
 class Market < ActiveRecord::Base
   has_many :vendors
 
-def self.search(search)
-    where("name like ?", "%#{search}%")
-  end
+  scope :search, ->(search) { where("name like ?", "%#{search}%")}
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,16 +3,14 @@ class Vendor < ActiveRecord::Base
   has_many :sales
   has_many :products
 
+  scope :search, ->(search) { where("name like ?", "%#{search}%")}
+
   def self.total_sales(vendor)
     total_sales = 0
     vendor.sales.each do |sale|
       total_sales += sale.amount
     end
     total_sales
-  end
-
-  def self.search(search)
-    where("name like ?", "%#{search}%")
   end
 
 end
